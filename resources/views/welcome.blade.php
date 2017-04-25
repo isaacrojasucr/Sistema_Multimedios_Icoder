71f283d556a859a98f854098e387c1f6870e5bcd
kevsalazararaya <kevin.salazar@ucrso.info>
2017-04-25 12:15:32 -0600

    Prueba Kevin

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel - Multimedios
                 </div>
 
                 <div class="links">
