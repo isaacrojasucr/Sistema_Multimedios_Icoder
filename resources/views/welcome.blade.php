621e1d2d72b3f94049acae058f874372194e27fe
isaacrojasucr <jorgeisaac.rojas@ucrso.info>
2017-04-26 14:55:26 -0600

    dfdf

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -84,7 +84,7 @@
                 </div>
 
 
-                <img src="images/test.jpg">
+                <img src="images/icoder-logo.png" class="img-rounded">
                 {{--<div class="links">--}}
                     {{--<a href="https://laravel.com/docs">Documentation</a>--}}
                     {{--<a href="https://laracasts.com">Laracasts</a>--}}
