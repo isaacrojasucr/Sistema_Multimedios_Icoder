ce2d3c863f26595a9f0528eb8b7689f0c0b10bda
isaacrojasucr <jorgeisaac.rojas@ucrso.info>
2017-05-10 15:21:40 -0600

    sss

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,17 +1,27 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-primary" style="text-align: center;">
-                <div class="panel-heading">Seleccione un deporte</div>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                <div class="panel panel-primary" style="text-align: center;">
+                    <div class="panel-heading">Seleccione un deporte</div>
 
-                <div class="panel-body">
+                    <div class="panel-body">
 
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
+    <script type="text/javascript">
+
+        $(document).ready(function () {
+            $('#option1').remove();
+            $('#option2').remove();
+            $('#option3').remove();
+            $('#option4').remove();
+        });
+
+    </script>
 @endsection
